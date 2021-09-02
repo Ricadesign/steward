ae7bf5432bc6afde2b8dd62ea97d242e797b1f72
David <dluque@rica.design>
2021-09-02 15:23:26 +0200

    fix: migrations not being run before tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace Ricadesign\Steward\Tests;
 
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
-use Illuminate\Foundation\Application;
 use Ricadesign\Steward\Table;
+use Illuminate\Support\Facades\Artisan;
 
 /**
  * Override the standard PHPUnit testcase with the Testbench testcase
@@ -25,6 +25,19 @@ abstract class TestCase extends OrchestraTestCase
         return [
             \Ricadesign\Steward\Providers\StewardServiceProvider::class
         ];
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        // Import the migrations
+        include_once str_replace('\\', '/', __DIR__ . '/../database/migrations/2021_08_23_155531_create_tables_table.php');
+        include_once str_replace('\\', '/', __DIR__ . '/../database/migrations/2021_08_23_175709_create_bookings_table.php');
+        include_once str_replace('\\', '/', __DIR__ . '/../database/migrations/2021_08_23_182159_create_booking_table_table.php');
+
+        // Run them
+        (new \CreateTablesTable)->up();
+        (new \CreateBookingsTable)->up();
+        (new \CreateBookingTableTable)->up();
     }
 
     protected function setUp(): void
